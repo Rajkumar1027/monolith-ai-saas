89fe25beb5e18b0eca40c21adeeb737c73460a84
Rajkumar1027 <grajkumar2021@gmail.com>
2026-03-29 02:28:24 +0530

    Memory optimization for 512MB RAM

--- project/ai/sentiment.py
+++ project/ai/sentiment.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-sentiment_model = pipeline("sentiment-analysis")
+sentiment_model = pipeline("sentiment-analysis", low_cpu_mem_usage=True)
 
 def analyze_sentiment(text):
     return sentiment_model(text)
